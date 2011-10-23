1a140f9347f1fb4fc90dc5f7ca3c1949cb6afecb
Ubuntu <ubuntu@ip-10-68-38-147.ec2.internal>
2011-10-23 17:33:39 +0000

    Add UI + request templates

--- mosaic/main/urls.py
+++ mosaic/main/urls.py
@@ -7,7 +7,10 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('mosaic.main.views',
     # Examples:
     # url(r'^$', 'mosaic.views.home', name='home'),
-    url(r'^/', include('home')),
+    url(r'^$', 'home', name='home'),
+    url(r'^request/(?P<rid>\d+)/$', 'req', name='req'),
+    url(r'^request/new/$', 'new', name='new'),
+    url(r'^request/$', 'req', name='req'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- mosaic/main/views.py
+++ mosaic/main/views.py
@@ -1,14 +1,19 @@
 # Create your views here.
 from main.models import *
+from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
 
+@csrf_exempt
 def req(request, rid=None):
     if request.method == "POST":
         bbox = request.POST['bbox']
-        r = Request(bbox)
+        r = Request(bbox=bbox)
         r.save()
-        return HttpResponseRedirect("/req/%s/" % r.id)
+        return HttpResponseRedirect("/request/%s/" % r.id)
     if rid:
         r = Request.objects.get(pk=rid)
         return render_to_response("request.html", {'request': r})
         
-        
+def new(request):
+    return render_to_response("ui.html")
